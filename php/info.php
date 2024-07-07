6acc62de67406dee00508cadd0157942d7be6800
CUZAMARK <110730813+CUZAMARK@users.noreply.github.com>
2024-07-07 13:23:13 +0530

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,9 +1,8 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = ""; // add the api here with the '/' at the end!
-$consumet = ""; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
+$apiLink = "https://animeapi-9qlo.onrender.com"; // add the api here with the '/' at the end!
 if (date("d") > 15){
-    $apiLink = ""; // same goes for here
+    $apiLink = "https://animeapi-9qlo.onrender.com"; // same goes for here
 }
 ?>
